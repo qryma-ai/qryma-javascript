3d6d82f0b050c76ff0b6d40772c58b122bd970da
陈康 <462121880@qq.com>
2026-03-18 17:49:43 +0800

    fix: qryma-ai

--- src/version.ts
+++ src/version.ts
@@ -5,4 +5,4 @@ export const __title__ = "qryma-javascript";
 export const __description__ = "Qryma Search API JavaScript SDK";
 export const __author__ = "Qryma Team";
 export const __license__ = "MIT";
-export const __url__ = "https://github.com/qryma/qryma-javascript";
+export const __url__ = "https://github.com/qryma-ai/qryma-javascript";
